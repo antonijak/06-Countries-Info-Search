ca163bfa2029d1a9ef56e04c7cdc9df10548c35a
Antonija <antonija.kasum@gmail.com>
2018-09-14 11:31:35 +0300

    Some CSS added

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let total = document.querySelector('#total');
 total.textContent = countries.length;
 
 //generate random hexadecimal number
-function randomHexaNumberGenerator1 (){
+function randomHexaNumberGenerator (){
   let characters = '123456789abcdef'  
   let hex = '#';
   for (i = 0; i < 6; i++){
@@ -15,69 +15,64 @@ function randomHexaNumberGenerator1 (){
   return hex
 }
 
-// create color div with country name inside it
+// creates color div with country name inside it
 function createCountryDiv (country){
   let colorDiv = document.createElement('div');
-  main.appendChild(colorDiv);
-  let hexValue = randomHexaNumberGenerator1();
+  let hexValue = randomHexaNumberGenerator();
   let countryName = document.createElement('div');
+  main.appendChild(colorDiv);
   colorDiv.appendChild(countryName);
   colorDiv.className = 'country';
-  countryName.textContent = country;
-  countryName.style.color = randomHexaNumberGenerator1();
-  countryName.className = 'country-name';
   colorDiv.style.backgroundColor = hexValue;
+  countryName.textContent = country;
+  countryName.style.color = randomHexaNumberGenerator();
+  countryName.className = 'country-name';
 }
 
-//generates countries from the countries array
+//generates countries from the countries array by first word
 function giveCountryNameFirst (){
   main.innerHTML = '';
   let matchWord = document.querySelector('#search').value.toUpperCase();
-  console.log(matchWord);
   let resultObject = {};
-  const whatLetter = countries.filter(function(val){
+  const whatLetter = countries.filter((val) => {
     let upperVal = val.toUpperCase();
     let match = upperVal.startsWith(matchWord);
     if (match){
     return upperVal}
   }); 
-  let key = whatLetter.length  
+  let key = whatLetter.length;  
+  resultObject[key] = whatLetter; 
+  for (country of resultObject[key]){
+    createCountryDiv(country.toUpperCase());
+  }
+  document.querySelector('#number').textContent = key;
+}
+
+const searchFirst = document.querySelector('#starting-word');
+searchFirst.addEventListener('click',giveCountryNameFirst);
+
+//generates countries from the countries array by any part
+function giveCountryNameAny (){
+  main.innerHTML = '';
+  let matchWord = document.querySelector('#search').value.toUpperCase();
+  console.log(matchWord);
+  let resultObject = {};
+  const whatLetter = countries.filter((val) => {
+    let upperVal = val.toUpperCase();
+    let match = upperVal.includes(matchWord);
+    if (match){
+    return upperVal}
+  }); 
+  let key = whatLetter.length;  
   document.querySelector('#number').textContent = key;
   resultObject[key] = whatLetter; 
   for (country of resultObject[key]){
-  createCountryDiv(country.toUpperCase());
+    createCountryDiv(country.toUpperCase());
   }
-  matchWord = '';
 }
 
-  const searchFirst = document.querySelector('#starting-word');
-  searchFirst.addEventListener('click',giveCountryNameFirst);
+const searchAny = document.querySelector('#any-word');
+searchAny.addEventListener('click',giveCountryNameAny);
 
-  function giveCountryNameAny (){
-    main.innerHTML = '';
-    let matchWord = document.querySelector('#search').value.toUpperCase();
-    console.log(matchWord);
-    let resultObject = {};
-    const whatLetter = countries.filter(function(val){
-      let upperVal = val.toUpperCase();
-      let match = upperVal.includes(matchWord);
-     console.log(match);
-      console.log(upperVal);
-      
-      if (match){
-      return upperVal}
-    }); 
-    let key = whatLetter.length;  
-    document.querySelector('#number').textContent = key;
-    resultObject[key] = whatLetter; 
-    for (country of resultObject[key]){
-    createCountryDiv(country.toUpperCase());
-    
-    }
-    
-    matchWord = '';
-  }
-
-
-  const searchAny = document.querySelector('#any-word');
-  searchAny.addEventListener('click',giveCountryNameAny);
+const search = document.querySelector('#search');
+search.addEventListener('input', giveCountryNameFirst)
