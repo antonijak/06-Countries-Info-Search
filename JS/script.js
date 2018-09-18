a43374f75e2c5718227878e33f8ffc660110be75
Antonija <antonija.kasum@gmail.com>
2018-09-18 14:28:31 +0300

    Some refactoring

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,5 @@
 
 let main = document.querySelector('main');
-
 let total = document.querySelector('#total');
 total.textContent = countries.length;
 
@@ -15,64 +14,60 @@ function randomHexaNumberGenerator (){
   return hex
 }
 
-// creates color div with country name inside it
+
+// creates color div with country's name inside it
 function createCountryDiv (country){
   let colorDiv = document.createElement('div');
-  let hexValue = randomHexaNumberGenerator();
   let countryName = document.createElement('div');
+  let hexValue = randomHexaNumberGenerator();
   main.appendChild(colorDiv);
   colorDiv.appendChild(countryName);
   colorDiv.className = 'country';
   colorDiv.style.backgroundColor = hexValue;
-  countryName.textContent = country;
+  countryName.textContent = country.toUpperCase();
   countryName.style.color = randomHexaNumberGenerator();
   countryName.className = 'country-name';
 }
 
-//generates countries from the countries array by first word
-function giveCountryNameFirst (){
+
+function searchByFirst (){
   main.innerHTML = '';
-  let matchWord = document.querySelector('#search').value.toUpperCase();
-  let resultObject = {};
-  const whatLetter = countries.filter((val) => {
-    let upperVal = val.toUpperCase();
-    let match = upperVal.startsWith(matchWord);
-    if (match){
-    return upperVal}
+  let userInputUpper = document.querySelector('#search').value.toUpperCase();
+  //returns an array of all countries that match
+  const resultCountries = countries.filter((val) => {
+    let upperCaseVal = val.toUpperCase();
+    let match = upperCaseVal.startsWith(userInputUpper);
+    if (match){ return upperCaseVal }
   }); 
-  let key = whatLetter.length;  
-  resultObject[key] = whatLetter; 
-  for (country of resultObject[key]){
-    createCountryDiv(country.toUpperCase());
-  }
-  document.querySelector('#number').textContent = key;
+  //generates country div for each of them
+  resultCountries.forEach( (country) => { createCountryDiv(country);} ) 
+  //shows number of matching countries
+  document.querySelector('#number').textContent = resultCountries.length;
 }
 
-const searchFirst = document.querySelector('#starting-word');
-searchFirst.addEventListener('click',giveCountryNameFirst);
 
-//generates countries from the countries array by any part
-function giveCountryNameAny (){
+function searchByAny (){
   main.innerHTML = '';
-  let matchWord = document.querySelector('#search').value.toUpperCase();
-  console.log(matchWord);
-  let resultObject = {};
-  const whatLetter = countries.filter((val) => {
-    let upperVal = val.toUpperCase();
-    let match = upperVal.includes(matchWord);
-    if (match){
-    return upperVal}
+  let userInputUpper = document.querySelector('#search').value.toUpperCase();
+  //returns an array of all countries that match
+  const resultCountries = countries.filter((val) => {
+    let upperCaseVal = val.toUpperCase();
+    let match = upperCaseVal.includes(userInputUpper);
+    if (match){ return upperCaseVal }
   }); 
-  let key = whatLetter.length;  
-  document.querySelector('#number').textContent = key;
-  resultObject[key] = whatLetter; 
-  for (country of resultObject[key]){
-    createCountryDiv(country.toUpperCase());
-  }
+  //generates country div for each of them
+  resultCountries.forEach((country) => { createCountryDiv(country);}) 
+  //shows number of matching countries
+  document.querySelector('#number').textContent = resultCountries.length;
 }
 
-const searchAny = document.querySelector('#any-word');
-searchAny.addEventListener('click',giveCountryNameAny);
 
-const search = document.querySelector('#search');
-search.addEventListener('input', giveCountryNameFirst)
+//when user types, matching coutries are generated
+const searchField = document.querySelector('#search');
+searchField.addEventListener('input', searchByFirst)
+//when user clicks search by first word, matching coutries are generated
+const searchFirstBtn = document.querySelector('#starting-word');
+searchFirstBtn.addEventListener('click', searchByFirst);
+//when user clicks search by any word, matching coutries are generated
+const searchAnyBtn = document.querySelector('#any-word');
+searchAnyBtn.addEventListener('click', searchByAny);
